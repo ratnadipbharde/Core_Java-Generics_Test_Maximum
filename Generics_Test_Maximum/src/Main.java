fbb826861516bb91a1af7183100e129bf9e7c25e
Ratnadip Bharde <ratnadipbharde@gmail.com>
2022-11-29 21:36:30 +0530

    Added Given 3 Strings find the maximum

--- Generics_Test_Maximum/src/Main.java
+++ Generics_Test_Maximum/src/Main.java
@@ -1,15 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Find Maximum Problem using Generics");
-        Integer a = 60;
-        Integer b = 90;
-        Integer c = 100;
-        Float d = 7.1f;
-        Float e = 8.1f;
-        Float f = 9.1f;
+        Integer i1 = 60;
+        Integer i2 = 90;
+        Integer i3 = 100;
+        Float f1 = 7.1f;
+        Float f2 = 8.1f;
+        Float f3 = 9.1f;
+        String s1="mumbai";
+        String s2="gujrat";
+        String s3="nagpur";
+
         Main main = new Main();
-        System.out.println("Max Number (Integer) is : " + main.checkMax(a, b, c));
-        System.out.println("Max Number (Float) is : " + main.checkMax(d, e, f));
+        System.out.println("Max Number (Integer) is : " + main.checkMax(i1, i2, 3));
+        System.out.println("Max Number (Float) is : " + main.checkMax(f1, f2, f3));
+        System.out.println("Max String (String) is : " + main.checkMax(s1, s2, s3));
     }
 
     private Integer checkMax(Integer a, Integer b, Integer c) {
@@ -23,6 +28,15 @@ public class Main {
 
     private Float checkMax(Float a, Float b, Float c) {
         Float MAX = a;
+        if (b.compareTo(MAX) > 0)
+            MAX = b;
+        if (c.compareTo(MAX) > 0)
+            MAX = c;
+        return MAX;
+    }
+
+    private String checkMax(String a, String b, String c) {
+        String MAX = a;
         if (b.compareTo(MAX) > 0)
             MAX = b;
         if (c.compareTo(MAX) > 0)
