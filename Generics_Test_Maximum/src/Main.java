dc507d9e0ddfd63277ff0d521d29178b08bec131
Ratnadip Bharde <ratnadipbharde@gmail.com>
2022-11-29 20:55:22 +0530

    Added Given 3 Integers find the maximum Solution

--- Generics_Test_Maximum/src/Main.java
+++ Generics_Test_Maximum/src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Find Maximum Problem using Generics");
+        Integer a = 40;
+        Integer b = 80;
+        Integer c = 30;
+        Main main = new Main();
+        System.out.println(main.checkMax(a, b, c));
+    }
+
+    private Integer checkMax(Integer a, Integer b, Integer c) {
+        Integer MAX=a;
+        if (b.compareTo(MAX) > 0)
+            MAX = b;
+        if (c.compareTo(MAX) > 0)
+            MAX = c;
+        return MAX;
     }
 }
 
