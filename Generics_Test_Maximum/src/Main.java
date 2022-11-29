011ebd459528c279e4d8be3cf1da5dc84b3577c0
Ratnadip Bharde <ratnadipbharde@gmail.com>
2022-11-29 21:04:00 +0530

    TC1.3 Given Max Number at 3rd Position return the Same Number test  successfully test

--- Generics_Test_Maximum/src/Main.java
+++ Generics_Test_Maximum/src/Main.java
@@ -3,9 +3,9 @@ public class Main {
         System.out.println("Find Maximum Problem using Generics");
         Integer a = 60;
         Integer b = 90;
-        Integer c = 40;
+        Integer c = 100;
         Main main = new Main();
-        System.out.println(main.checkMax(a, b, c));
+        System.out.println("Max Number is : "+main.checkMax(a, b, c));
     }
 
     private Integer checkMax(Integer a, Integer b, Integer c) {
