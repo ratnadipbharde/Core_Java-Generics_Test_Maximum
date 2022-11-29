1bcc591f5400890b6ff44b08f8d3c5eb377aa803
Ratnadip Bharde <ratnadipbharde@gmail.com>
2022-11-29 21:20:18 +0530

    TC2.2 test given 2nd position number is Max

--- Generics_Test_Maximum/src/Main.java
+++ Generics_Test_Maximum/src/Main.java
@@ -4,24 +4,16 @@ public class Main {
         Integer a = 60;
         Integer b = 90;
         Integer c = 100;
-        Float d=7.1f;
-        Float e=5.6f;
-        Float f=6.4f;
+        Float d = 7.1f;
+        Float e = 8.1f;
+        Float f = 6.4f;
         Main main = new Main();
-        System.out.println("Max Number (Integer) is : "+main.checkMax(a, b, c));
-        System.out.println("Max Number (Float) is : "+main.checkMax(d, e, f));
+        System.out.println("Max Number (Integer) is : " + main.checkMax(a, b, c));
+        System.out.println("Max Number (Float) is : " + main.checkMax(d, e, f));
     }
 
     private Integer checkMax(Integer a, Integer b, Integer c) {
-        Integer MAX=a;
-        if (b.compareTo(MAX) > 0)
-            MAX = b;
-        if (c.compareTo(MAX) > 0)
-            MAX = c;
-        return MAX;
-    }
-    private Float checkMax(Float a, Float b, Float c) {
-        Float MAX=a;
+        Integer MAX = a;
         if (b.compareTo(MAX) > 0)
             MAX = b;
         if (c.compareTo(MAX) > 0)
@@ -29,5 +21,13 @@ public class Main {
         return MAX;
     }
 
+    private Float checkMax(Float a, Float b, Float c) {
+        Float MAX = a;
+        if (b.compareTo(MAX) > 0)
+            MAX = b;
+        if (c.compareTo(MAX) > 0)
+            MAX = c;
+        return MAX;
+    }
 }
 
